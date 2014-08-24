f7f7c4e438d3b63720f7e59c6d5d12529e18fbeb
Emiliano Busiello <emilianobusiello@gmail.com>
2014-08-24 14:10:18 +0200

    2

--- src/main/scala/scalaE/PathDependentType.scala
+++ src/main/scala/scalaE/PathDependentType.scala
@@ -18,7 +18,29 @@ class PathDependentType {
   val c2: b2.Coordinate = b2.Coordinate(25, 25)
   b1.occupied += c1
   b2.occupied += c2
+
   // Next line doesn't compile
   //b1.occupied += c2
 
+
+  class outer {
+    def getInner = new inner()
+
+    class inner {}
+
+  }
+
+  // # syntax is called type selection
+  val i: outer#inner = new outer().getInner
+  val p: PathDependentType.this.type#outer = new outer()
+
+  /*
+  Note that this is conceptually different from a path dependent type p.Inner, where the path p denotes a value (since the path is dependent on the value instanciated), not a type.
+  Consequently, the type expression Outer # t is not well-formed if t is an abstract type defined in Outer.
+
+
+  In fact, path dependent types in Scala can be expanded to type selections. The path dependent type p.t is taken as a shorthand for p.type # t.
+  Here, p.type is a singleton type, which represents just the object denoted by p.
+
+   */
 }
---
+++ src/main/scala/scalaE/Variance.scala
@@ -0,0 +1,45 @@
+package scalaE
+
+class Variance {
+
+  /**
+   *
+   * covariant for the types of immutable fields and method results
+   * contravariant for method argument types and upper type parameter bounds
+   *
+   */
+  abstract class GenList[+T] {
+    def isEmpty: Boolean
+
+    def head: T
+
+    def tail: GenList[T]
+
+    def prepend[S >: T](x: S): GenList[S] = new Cons(x, this)
+
+  /*
+    T is controvariatn position because it appears in the method signature (x: T)
+    def prepend(x: (-)T): GenList[(+)T] =
+      new Cons(x, this)
+   */
+
+  }
+
+
+  object Empty extends GenList[Nothing] {
+    def isEmpty: Boolean = true
+
+    def head: Nothing = throw new Error("Empty.head")
+
+    def tail: GenList[Nothing] = throw new Error("Empty.tail")
+  }
+
+  class Cons[+T](x: T, xs: GenList[T]) extends GenList[T] {
+    def isEmpty: Boolean = false
+
+    def head: T = x
+
+    def tail: GenList[T] = xs
+  }
+
+}
