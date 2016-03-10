c27c41e52fb4ecf3100daec28b07a91cd844e94e
Emiliano Busiello <emiliano@relayr.de>
2016-03-10 11:50:39 +0100

    Meaningful applicative example.

--- src/main/scala/scalaE/scalaze/ApplicativeEX.scala
+++ src/main/scala/scalaE/scalaze/ApplicativeEX.scala
@@ -1,101 +1,62 @@
 package scalaE.scalaze
 
-import scala.collection.immutable.IndexedSeq
-import scala.collection.mutable
+import scala.language.higherKinds
 import scalaz.Applicative
 
+/**
+  * From https://github.com/scalaz/scalaz/blob/series/7.3.x/example/src/main/scala/scalaz/example/ApplyUsage.scala
+  */
 object ApplicativeEx {
 
+  import scalaz.Apply
+  import scalaz.std.anyVal._
+  import scalaz.std.list._
+  import scalaz.std.option._
+  import scalaz.std.string._
+  import scalaz.syntax.equal._
+  import scalaz.syntax.std.option._
 
-  val apOpt = new Applicative[Option] {
-    override def point[A](a: => A): Option[A] = Some(a)
+  val intToString: Int => String = _.toString
+  val double: Int => Int = _ * 2
+  val addTwo: Int => Int = _ + 2
 
-    override def ap[A, B](fa: => Option[A])(f: => Option[(A) => B]): Option[B] =
+  // basically directly apply functions to applications
+
+  // map
+  assert(Apply[Option].map(1.some)(intToString) === "1".some) // like Option(1).map(intToString)
+  assert(Apply[Option].map(1.some)(double) === 2.some)
+  assert(Apply[Option].map(none)(double) === none)
+
+  // ap is different, the function applied is a functor function F[A => B] so a functor which holds
+  // a function from A to B, in case of option, if that functor holds a none, a non is returned.
+  assert(Apply[Option].ap(1.some)(some(intToString)) === "1".some)
+  assert(Apply[Option].ap(1.some)(some(double)) === 2.some)
+  assert(Apply[Option].ap(none)(some(double)) === none)
+  assert(Apply[Option].ap(1.some)(none[Int => Int]) === none[Int])
+  assert(Apply[Option].ap(none)(none[Int => Int]) === none[Int])
+  assert(Apply[List].ap(List(1,2,3))(List(double, addTwo)) === List(2,4,6,3,4,5))
+
+  // custom applicative for custom class example.
+  trait Super {}
+  case class Child[A](val param: List[A]) extends Super {
+
+    final def map[B](f: A => B): Child[B] =
+      Child[B](param.map(f))
+
+    final def flatMap[B](f: A => Child[B]): Child[B] = {
+      param.map(f).foldLeft(Child[B](List()))((acc, curr) => {
+        Child[B](acc.param ++ curr.param)
+      })
+    }
+  }
+
+  val apB = new Applicative[Child] {
+    override def point[A](a: => A): Child[A] = Child(List(a))
+
+    override def ap[A, B](fa: => Child[A])(f: => Child[(A) => B]): Child[B] =
       f.flatMap(func => fa.map(func))
   }
 
-  apOpt.ap(Some("123"))(Some((_: String) + ""))
-
-  val str2 = "LEMELEVEL".tails.toList.init
-
-  def loopSubWord(word: String, acc: Seq[String]): Seq[String] = {
-    def inLoop(string: String): Seq[String] = {
-
-      var w: String = string.head.toString
-      var l = mutable.Seq.empty[String]
-
-      string.tail.foreach(c => {
-        w = w + c
-        if (w == w.reverse) l +: w
-      })
-      l
-    }
-
-    if (word.isEmpty) acc
-    else loopSubWord(word.tail, acc ++ inLoop(word))
-  }
-
-  str2.flatMap(subword => {
-    loopSubWord(subword, List())
-  }).distinct
-
-
-  val str = "LEMELEVEL".toList
-  for {
-    i <- 1 to str.length
-    pairs <- str.sliding(i)
-    if pairs == pairs.reverse
-  } yield pairs
-
-
-
-  def wordCombinations(word: String) = {
-    def loop(prefix: String, w: String, acc: List[String]): List[String] = w.isEmpty match {
-      case false =>
-        val combination = prefix + w.head
-        if(isPaly(combination)) loop(combination, w.tail, acc :+ combination)
-        else loop(combination, w.tail, acc)
-      case true =>
-        acc
-    }
-
-    def isPaly(word: String): Boolean = {
-      println(word)
-      word == word.reverse
-    }
-
-    loop("", word, List())
-  }
-
-
-  val w = "LEMEL".toList
-
-  for {
-    i <- 1 to w.length
-    slided <- w.sliding(i)
-    if slided == slided.reverse
-  } yield slided
-
-  val paliWord = "LEMELEVEL".tails.toList
-
-  val palies: List[String] = paliWord.flatMap(word => wordCombinations(word)).distinct
-
-  def removeChar(word: String, c: Char): String = {
-    word.filter(_ == c)
-  }
-
-  // How to find first non repeated character of a given String?
-  def findNonRepeated(word: String): Option[Char] = {
-    println(word)
-    if (word.isEmpty) None
-    else {
-      val head = word.head
-      val tail = word.tail
-      tail.find(_ == head) match {
-        case Some(char) => findNonRepeated(tail.filter(_ != head))
-        case None => Some(head)
-      }
-    }
-  }
+  lazy val applicate = apB.ap(Child(List(1,2,3)))(Child(List(double, addTwo)))
 
 }
